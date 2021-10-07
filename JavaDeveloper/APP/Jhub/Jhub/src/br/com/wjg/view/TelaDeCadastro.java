48ba102640cdbdbef0ff66d7ce067904eecb16a2
NickMuz <60697606+NickMuz@users.noreply.github.com>
2021-10-06 21:18:43 -0300

    Threads

--- JavaDeveloper/APP/Jhub/Jhub/src/br/com/wjg/view/TelaDeCadastro.java
+++ JavaDeveloper/APP/Jhub/Jhub/src/br/com/wjg/view/TelaDeCadastro.java
@@ -858,8 +858,8 @@ public class TelaDeCadastro {
 
 
 						Thread threadCalculo =  new Thread(result, "Thread Calculador");
-                        getJtresult().setText(result.toString()); 
-                        if(result.toString().substring(0,5).equals("fatal")) { new Mensagens(result.toString()); return; }
+                        getJtresult().setText(threadCalculo.toString()); 
+                        if(threadCalculo.toString().substring(0,5).equals("fatal")) { new Mensagens(result.toString()); return; }
                         new Mensagens("Arquivo enviado para o repositorio!");
 						threadCalculo.start();
 

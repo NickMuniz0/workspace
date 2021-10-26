0e7fa7fbb7ca941fd5ca8bdfc25e28cc7d49d581
NickMuz <60697606+NickMuz@users.noreply.github.com>
2021-10-26 00:15:47 -0300

    JDBC

--- JavaDeveloper/Web/loja-virtual-repository/src/main/java/TestaListagem.java
+++ JavaDeveloper/Web/loja-virtual-repository/src/main/java/TestaListagem.java
@@ -1,16 +1,17 @@
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 
 public class TestaListagem {
 	public static void main(String[] args) throws SQLException {
 		
 		ConnectionFactory criaConexao = new ConnectionFactory();
 		Connection connection = criaConexao.recuperarConexao();
-		
-		Statement stm = connection.createStatement();
-		stm.execute("Select ID,NOME,DESCRICAO FROM PRODUTO");
+		PreparedStatement stm = connection.prepareStatement("Select ID,NOME,DESCRICAO FROM PRODUTO");
+		stm.execute();
+		//Statement stm = connection.createStatement();
+		//stm.execute("Select ID,NOME,DESCRICAO FROM PRODUTO");
 		ResultSet rst = stm.getResultSet();
 		while(rst.next()) {
 			Integer	id = rst.getInt("ID");
---
+++ JavaDeveloper/Web/loja-virtual-repository/src/main/java/modelo/Produto.java
@@ -0,0 +1,39 @@
+package modelo;
+
+public class Produto {
+		
+		private Integer  id;
+		private String nome; 
+		private String descricao;
+		
+		public Produto(String nome, String descricao) {
+			this.nome = nome;
+			this.descricao = descricao;
+		}
+		
+		public Produto(Integer id,String nome, String descricao) {
+			this.id= id;
+			this.nome = nome;
+			this.descricao = descricao ;
+		}
+		
+		public void setId(int id) {
+			this.id =id;
+		}
+
+		public String getNome() {
+			return this.nome;
+		}
+		
+		
+		public String getDescricao() {
+			return this.descricao;
+		}
+		
+		@Override
+		public String toString() {
+		return String.format(" O produto criado é: %d, %s, %s", this.id,this.nome, this.descricao);
+		}
+
+	
+}

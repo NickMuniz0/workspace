c99d3139df458e066ca110d9fb240f03c37d4809
NickMuz <nickmuzi@outlook.com>
2024-07-15 22:11:23 -0300

    Remove Unicode

--- Python/TRADUZIR CARACTERS ESPECIAIS NO DICT-JSON/main.py
+++ Python/TRADUZIR CARACTERS ESPECIAIS NO DICT-JSON/main.py
@@ -1,5 +1,6 @@
-texto = '{"body":{"dados_fixos":{"T1":{"C1":"^"},"T2":"¢","T3":{"C1":{"D1":"!","D2":"¬"}}},"dados_variaveis":[{"Teste1":"!^"},{"Teste2":"^"}]}}'
-texto_antigo = '{"body":{"dados_fixos":{"T1":{"C1":"^"},"T2":"¢","T3":{"C1":{"D1":"!","D2":"¬"}}},"dados_variaveis":[{"Teste1":"!^"},{"Teste2":"^"}]}}'
+import re
+texto = '{"body":{"dados_fixos":{"T1":{"C1":"^"},"T2":"¢","T3":{"C1":{"D1":"!","D2":"¬"}}},"dados_variaveis":[{"Teste1":"!^"},{"Teste2":"^"},{"Teste3":"A\\u0014"}]}}'
+texto_antigo = '{"body":{"dados_fixos":{"T1":{"C1":"^"},"T2":"¢","T3":{"C1":{"D1":"!","D2":"¬"}}},"dados_variaveis":[{"Teste1":"!^"},{"Teste2":"^"},{"Teste3":"A\\u0014"}]}}'
 char_to_replace={
                     ord(']'):'!',
                     ord('^'):'¬',
@@ -11,6 +12,7 @@ char_to_replace={
 ultimo= texto[-3:]
 texto=texto.translate(char_to_replace)
 texto= texto.replace(texto[-3:],ultimo).replace(':¢',':[')
+texto = re.sub(r'([\\u00-\\u7F]{6,8})','',texto) # Remove Unicode
 print(texto)
 print(texto_antigo)
-# print(hex(ord('[')))
+print(hex(ord('\u0014')))
